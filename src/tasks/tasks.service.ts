2c0d98ba70683496cc467a0b547930a712cef897
alvalenda <banysan@msn.com>
2022-12-15 08:43:20 -0300

    feat(tasks): implement `[GET]` getAll route

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+}
