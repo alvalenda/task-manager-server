f74641e8d6b49c1619e1705a11bcf457cddf2ba4
alvalenda <banysan@msn.com>
2022-12-17 20:28:10 -0300

    feat(swagger): implement `swagger` on `tasks` route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Delete, Patch, Query, UsePipes } from '@nestjs/common/decorators';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger/dist';
 import { TaskStatusValidationPipe } from 'src/common/decorators/validation/task-status-validaton.pipe';
 import { handleError } from 'src/common/helpers/http-exception.filter';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -15,11 +16,19 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task, TaskStatus } from './entities/tasks.entity';
 import { TasksService } from './tasks.service';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all tasks',
+    description:
+      'Get all tasks. The seach can be filtered via query arguments "search" and "status". It is optional to provide both arguments.',
+  })
+  @ApiQuery({ name: 'status', required: false })
+  @ApiQuery({ name: 'search', required: false })
   getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilters(filterDto);
@@ -29,11 +38,20 @@ export class TasksController {
   }
 
   @Get('/:id')
+  @ApiOperation({
+    summary: 'Get task by ID',
+    description: 'Get task by ID',
+  })
   getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskById(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a new task',
+    description:
+      'Create a new task with a title and a description. The status is set to "OPEN" by default.',
+  })
   @UsePipes(ValidationPipe)
   createTask(@Body() dto: CreateTaskDto): Task {
     try {
@@ -44,6 +62,11 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
+  @ApiOperation({
+    summary: 'Update one task status by ID',
+    description:
+      'Update one task status by ID. The status can be "OPEN", "IN_PROGRESS" or "DONE".',
+  })
   updateTaskStatus(
     @Param('id') id: string,
     @Body('status', TaskStatusValidationPipe) taskStatus: TaskStatus,
@@ -52,12 +75,21 @@ export class TasksController {
   }
 
   @Patch('/:id')
+  @ApiOperation({
+    summary: 'Update one task by ID',
+    description:
+      'Update one task by ID. The title and the description can be updated.',
+  })
   @UsePipes(new ValidationPipe({ transform: true }))
   updateTask(@Param('id') id: string, @Body() dto: UpdateTaskDto): Task {
     return this.tasksService.updateTask(id, dto);
   }
 
   @Delete('/:id')
+  @ApiOperation({
+    summary: 'Delete one task by ID',
+    description: 'Delete one task by ID. The task will be deleted permanently.',
+  })
   deleteTask(@Param('id') id: string): void {
     this.tasksService.deleteTask(id);
   }
