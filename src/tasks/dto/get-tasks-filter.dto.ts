6ce933e04e3f401d669c5889e6a4e0ffea25f54e
alvalenda <banysan@msn.com>
2022-12-17 19:44:15 -0300

    feat(tasks): add validation to `GetTasksFilterDto`

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsIn, IsOptional } from 'class-validator';
+import { IsNotEmpty } from 'class-validator/types/decorator/decorators';
 import { TaskStatus } from '../entities/tasks.entity';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search?: string;
 }
