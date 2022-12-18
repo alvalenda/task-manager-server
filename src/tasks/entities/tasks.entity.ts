35187a9e98a35c4ab1d6302054193cef455f4473
alvalenda <banysan@msn.com>
2022-12-18 09:06:01 -0300

    feat(tasks): create new tasks.entity

--- src/tasks/entities/tasks.entity.ts
+++ src/tasks/entities/tasks.entity.ts
@@ -1,12 +1,29 @@
-export class Task {
-  id: string;
-  title: string;
-  description: string;
-  status: TaskStatus;
-}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TaskStatus } from '../model/tasks.model';
 
-export const enum TaskStatus {
-  OPEN = 'OPEN',
-  IN_PROGRESS = 'IN_PROGRESS',
-  DONE = 'DONE',
+@Entity()
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+
+  //   @ManyToOne(type => User, user => user.tasks, { eager: false })
+  //   user: User;
+
+  //   @Column()
+  //   userId: number;
 }
