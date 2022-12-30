259d2fcba23d92e2220dc5d579734290d046ee2c
alvalenda <banysan@msn.com>
2022-12-30 04:37:30 -0300

    fix(tasks): fix `TypeOrmModule` forFeature

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/tasks.entity';
 import { TasksController } from './tasks.controller';
-import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
   providers: [TasksService],
 })
