4f04bff2db8efa666c709e1b38223e090d0e06d8
alvalenda <banysan@msn.com>
2023-01-04 10:29:07 -0300

    feat(auth): implement `Jwt Strategy`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: String(process.env.JWT_SECRET), // needs to be transformed into a string here, otherwise it will be undefined in the jwt module. I don't know why.
+      signOptions: { expiresIn: '1d' },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
