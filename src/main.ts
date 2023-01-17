699112f574c2a703b95708906e61b992948717f7
alvalenda <banysan@msn.com>
2023-01-17 20:39:28 -0300

    feat(config): add config to main

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const logger = new Logger('bootstrap');
+  const serverConfig = config.get('server');
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Tasks Manager API')
     .setDescription('API for managing tasks')
     .setVersion('1.0')
@@ -22,10 +24,10 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || serverConfig.port;
 
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
