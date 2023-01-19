aac862b4fb264e0380300731dee40f97ae775976
alvalenda <banysan@msn.com>
2023-01-19 12:28:24 -0300

    feat(config): create `serverConstants`

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -1,6 +1,21 @@
 import * as config from 'config';
 
-export const jwtConstants = {
+export const jwtConstants: JwtConstants = {
   secret: process.env.JWT_SECRET,
   expiresIn: config.get('jwt.expiresIn'),
+};
+
+export const serverConstants: ServerConstants = {
+  port: process.env.PORT || config.get('server.port'),
+  origin: process.env.ORIGIN || config.get('server.origin'),
+};
+
+type ServerConstants = {
+  port: number | string;
+  origin: string;
+};
+
+type JwtConstants = {
+  secret: string;
+  expiresIn: string;
 };
