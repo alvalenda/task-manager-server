50ba11e3757a736ea75b91310cf6e1da0c15f643
alvalenda <banysan@msn.com>
2023-02-09 07:28:20 -0300

    feat(user): implement `Patch` & `Delete` in user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,30 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
+  Logger,
+  Patch,
   Post,
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/common/decorators/requests/get-user.decorator';
 import { handleError } from 'src/common/helpers/http-exception.filter';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UpdateUserNameDto } from './dto/update-user-name.dto';
 import { User } from './entities/user.entity';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger('AuthController');
+
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
@@ -58,6 +66,47 @@ export class AuthController {
     description: 'Check if user is signed in. Returns user data.',
   })
   async signed(@GetUser() user: User): Promise<Partial<User>> {
-    return { id: user.id, username: user.username };
+    return { id: user.id, username: user.username, name: user.name };
+  }
+
+  @Patch('/update')
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Update user name',
+    description: 'Update user name.',
+  })
+  @ApiBody({ type: UpdateUserNameDto })
+  async updateName(
+    @GetUser() user: User,
+    @Body('name') name: UpdateUserNameDto,
+  ): Promise<Partial<User>> {
+    try {
+      this.logger.debug(
+        `User "${user.username}" is updating name to "${name}"`,
+      );
+
+      return await this.authService.updateName(user.id, name);
+    } catch (err) {
+      handleError(err);
+    }
+  }
+
+  @Delete()
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Delete logged in user',
+    description: 'Delete logged in user.',
+  })
+  async delete(@GetUser() user: User): Promise<void> {
+    try {
+      this.logger.debug(`User "${user.username}" is deleting account`);
+
+      await this.authService.deleteUser(user.id);
+    } catch (err) {
+      handleError(err);
+    }
   }
 }
